f73cba5d3d79c971480c4a373738154ca1bde110
THA Hieu <tranhuynhanhhieu@gmail.com>
2024-09-18 22:06:01 +0700

    Pre factor Http request

--- src/main/java/com/devteria/identify_service/Exception/ErrorCode.java
+++ src/main/java/com/devteria/identify_service/Exception/ErrorCode.java
@@ -1,26 +1,29 @@
 package com.devteria.identify_service.Exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
 public enum ErrorCode {
-    USER_EXISTED(1001, "User already exists"),
-    USERNAME_INVALID(1002, "Username must be at least 3 characters"),
-    INVALID_PASSWORD(1003, "Password must be at least 3 characters"),
-    INVALID_KEY(1004, "Invalid key"),
-    USER_NOT_EXISTED(1005, "User not already exists"),
-    UNAUTHENTICATED(1006, "Unauthenticated"),
+    UNCATEGORIZED_EXCEPTION(9999,"Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    USER_EXISTED(1001, "User already exists", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1002, "Username must be at least 3 characters", HttpStatus.BAD_REQUEST),
+    INVALID_PASSWORD(1003, "Password must be at least 3 characters", HttpStatus.BAD_REQUEST),
+    INVALID_KEY(1004, "Invalid key", HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTED(1005, "User not already exists", HttpStatus.NOT_ACCEPTABLE),
+    UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(1007, "You do not have permission", HttpStatus.FORBIDDEN),
     ;
-    ErrorCode(int code, String message) {
+
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
         this.message = message;
+        this.statusCode = statusCode;
     }
 
     private int code;
     private String message;
+    private HttpStatusCode statusCode;
 
-    public int getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
--- src/main/java/com/devteria/identify_service/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/devteria/identify_service/Exception/GlobalExceptionHandler.java
@@ -1,20 +1,24 @@
 package com.devteria.identify_service.Exception;
 
 import com.devteria.identify_service.DTO.Request.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.nio.file.AccessDeniedException;
+
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = RuntimeException.class)
     ResponseEntity<ApiResponse> handleRuntimeException(RuntimeException exception) {
         ApiResponse apiResponse = new ApiResponse();
 
-        apiResponse.setCode(1001);
-        apiResponse.setMessage(exception.getMessage());
+        apiResponse.setCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode());
+        apiResponse.setMessage(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage());
 
         return ResponseEntity.badRequest().body(apiResponse);
     }
@@ -22,13 +26,25 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = AppException.class)
     ResponseEntity<ApiResponse> handleAppException(AppException exception) {
         ErrorCode errorCode = exception.getErrorCode();
-
         ApiResponse apiResponse = new ApiResponse();
 
         apiResponse.setCode(errorCode.getCode());
         apiResponse.setMessage(errorCode.getMessage());
 
-        return ResponseEntity.badRequest().body(apiResponse);
+        return ResponseEntity.status(errorCode.getStatusCode())
+                .body(apiResponse);
+    }
+
+    @ExceptionHandler(value = AccessDeniedException.class)
+    ResponseEntity<ApiResponse> handlingAccessDeniedException(AccessDeniedException exception) {
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+
+        return ResponseEntity.status(errorCode.getStatusCode()).body(
+                ApiResponse.builder()
+                        .code(errorCode.getCode())
+                        .message(errorCode.getMessage())
+                        .build()
+        );
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
